b1c03f38d615dd1da97f85db9343cebb86a3b760
aerdem4 <ahmeterd4@gmail.com>
2020-03-13 09:28:59 +0100

    Add rolling and shifting cuda transform functions

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # rapids-kaggle-utils
 High level utility functions for using Rapids on Kaggle Competitions
+
+## cu_utils/transform
+Widely used group transform functions implemented in numba/cuda.
+They can be used within cudf's apply_grouped functions.
+Example notebooks:
+https://www.kaggle.com/aerdem4/ion-lofo-importance-on-gpu-via-rapids-xgboost
+https://www.kaggle.com/aerdem4/m5-lofo-feature-importance
--- cu_utils/transform.py
+++ cu_utils/transform.py
@@ -39,3 +39,37 @@ def cu_min_transform(x, y_out):
 
     for i in range(cuda.threadIdx.x, len(x), cuda.blockDim.x):
         y_out[i] = res[0]
+
+
+def get_cu_shift_transform(shift_by, null_val=-1):
+    def cu_shift_transform(x, y_out):
+        for i in range(cuda.threadIdx.x, len(x), cuda.blockDim.x):
+            y_out[i] = null_val
+            if 0 <= i-shift_by < len(x):
+                y_out[i] = x[i-shift_by]
+    return cu_shift_transform
+
+
+def get_cu_rolling_mean_transform(window, null_val=-1):
+    def cu_rolling_mean_transform(x, y_out):
+        for i in range(cuda.threadIdx.x, len(x), cuda.blockDim.x):
+            y_out[i] = 0
+            if i >= window-1:
+                for j in range(cuda.threadIdx.y, window, cuda.blockDim.y):
+                    cuda.atomic.add(y_out, i, x[i-j])
+                y_out[i] /= window
+            else:
+                y_out[i] = null_val
+    return cu_rolling_mean_transform
+
+
+def get_cu_rolling_max_transform(window, null_val=-1):
+    def cu_rolling_max_transform(x, y_out):
+        for i in range(cuda.threadIdx.x, len(x), cuda.blockDim.x):
+            y_out[i] = -math.inf
+            if i >= window-1:
+                for j in range(cuda.threadIdx.y, window, cuda.blockDim.y):
+                    cuda.atomic.max(y_out, i, x[i-j])
+            else:
+                y_out[i] = null_val
+    return cu_rolling_max_transform
